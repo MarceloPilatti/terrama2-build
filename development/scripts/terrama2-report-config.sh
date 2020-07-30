f99e5164e80c836fda208fb7e6840a97f7414b68
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-07-30 10:26:23 -0300

    .

--- development/scripts/terrama2-report-config.sh
+++ development/scripts/terrama2-report-config.sh
@@ -3,10 +3,10 @@
 cd /home/$USER
 
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-. "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
 
 nvm install 8
 
